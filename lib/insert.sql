976ca9babbfeb437aad059fbe7b2e8d545e292b5
Absalom <mrclabsalom@gmail.com>
2023-09-10 09:51:59 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Gentle', true),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', true),
+  ('Tabitha', 7, 'F', 'White', 'Calm', true),
+  ('Sergeant Brown', 8, 'M', 'Brown', 'Grumpy', true),
+  ('Melissa', 4, 'F', 'Honey', 'Friendly', true),
+  ('Grinch', 2, 'M', 'Green', 'Mischievous', true),
+  ('Wendy', 6, 'F', 'Blonde', 'Energetic', true),
+  (null, null, null, null, null, null);
